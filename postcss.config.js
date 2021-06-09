61038e2cd4d148052a1c47928b13382bd48eecc6
Chris Collins <chriscollins238@gmail.com>
2021-06-09 10:47:51 +0100

    Correct formatting using editorconfig

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = (ctx) => ({
-	plugins: {
-		'autoprefixer': {},
-		// Only run CSSNano in production
-		cssnano: ctx.env === "production" ? {
-			preset: "default"
-		} : false
-	},
-})
+  plugins: {
+    'autoprefixer': {},
+    // Only run CSSNano in production
+    cssnano: ctx.env === "production" ? {
+      preset: "default"
+    } : false
+  },
+});
